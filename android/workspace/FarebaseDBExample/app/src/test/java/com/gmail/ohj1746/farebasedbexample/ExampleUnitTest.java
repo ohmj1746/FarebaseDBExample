0c39cb1e3934e391100a5abbf23961365b810045
Oh jea Yong <ohj1746@gmail.com>
2017-09-14 21:03:48 +0900

    파이어베이스 프로젝트 시작

--- android/workspace/FarebaseDBExample/app/src/test/java/com/gmail/ohj1746/farebasedbexample/ExampleUnitTest.java
+++ android/workspace/FarebaseDBExample/app/src/test/java/com/gmail/ohj1746/farebasedbexample/ExampleUnitTest.java
@@ -2,7 +2,7 @@ package com.gmail.ohj1746.farebasedbexample;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
